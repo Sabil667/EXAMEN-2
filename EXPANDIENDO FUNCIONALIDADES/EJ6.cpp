9c76994737249face5dadca2fa6fb06f66ef4a94
Sabil667 <msabi@myuax.com>
2023-12-05 11:49:31 +0100

    .

--- EXPANDIENDO FUNCIONALIDADES/EJ6.cpp
+++ EXPANDIENDO FUNCIONALIDADES/EJ6.cpp
@@ -4,6 +4,16 @@
 
 class Entorno {
 public:
+    //Insertar nuevo símbolo en el entorno
+    void insert(const std::string& clave, int valor) {
+        auto it = entorno_.find(clave);
+        if (it != entorno_.end()) {
+            throw std::invalid_argument("La clave ya existe en el entorno.");
+        } else {
+            entorno_[clave] = valor;
+        }
+    }
+
 
 
 
