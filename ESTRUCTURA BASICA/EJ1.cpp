259dd1f54def11750474a43853b6d567a5633a8a
Sabil667 <msabi@myuax.com>
2023-12-05 11:03:11 +0100

    .

--- ESTRUCTURA BASICA/EJ1.cpp
+++ ESTRUCTURA BASICA/EJ1.cpp
@@ -15,6 +15,18 @@ public:
         symbols[name] = value;
     }
 
+    //Forma para obtener el valor de un simbolo de la tabla de simbolos.
+    int getSymbolValue(const std::string& name) const {
+        auto it = symbols.find(name);
+        if (it != symbols.end()) {
+            return it->second;
+        } else {
+            std::cerr << "Error: Symbol '" << name << "' not found." << std::endl;
+            // Puedes manejar el error de otra manera, esto es solo un ejemplo
+            return 0; // Valor predeterminado en caso de error
+        }
+    }
+
 
 
 
